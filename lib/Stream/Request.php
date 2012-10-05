93e5e0f7cf5833a8b70e5a69812f97f27996a797
rlt3 <leroy@bythehilt.com>
2012-10-04 19:05:06 -0500

    minor semantic fixes to Request.php

--- lib/Stream/Request.php
+++ lib/Stream/Request.php
@@ -1,16 +1,16 @@
 <?php
 class Request
 {
-   public static $path;
    public static $view;
    public static $method;
    public static $get;
+   private $path;
 
    public function __construct()
    {
       $this->_redirectSlashes();
 
-      self::$path   = explode('/', $_SERVER['REQUEST_URI']);
+      $this->path   = explode('/', $_SERVER['REQUEST_URI']);
       self::$method = strtolower($_SERVER['REQUEST_METHOD']);
       self::$view   = $this->_getView();
       self::$get    = $this->_getParameters();
@@ -20,13 +20,12 @@ class Request
 
    private function _getView()
    {
-      if(empty(self::$path[1])) return 'Index';
-      return ucfirst(self::$path[1]);
+      return (empty($this->path[1])) ? 'Index' : ucfirst($this->path[1]);
    }
 
    private function _getParameters()
    {
-      return array_slice(self::$path, 2);
+      return array_slice($this->path, 2);
    }
 
    private function _redirectSlashes()
