bc96de27115970e714016a3b70580fe0acc0a2ba
rlt3 <leroy@bythehilt.com>
2012-10-05 15:35:11 -0500

    Added the status codes for errors

--- lib/Stream/Error.php
+++ lib/Stream/Error.php
@@ -1,13 +1,12 @@
 <?php
 class Error
 {
+   protected $status = array( 404 => 'Page not found.',
+                              405 => 'Method not allowed.',
+                              400 => 'Bad request.');
+
    public function http($error)
    {
-      echo "Error: ", $error;
+      echo "Error: ", $error, ' - ', $this->status[$error];
    }
-
-   //public function __call($name, $arg)
-   //{
-   //   echo "Error: ", substr($name, 4);
-   //}
 }
